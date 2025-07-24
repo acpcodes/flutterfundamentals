e69b569d356243a7c80b644234a38ff77c71e736
ACParrilla <acparrilla.dev@gmail.com>
2025-07-24 22:08:58 +0800

    just another commit

--- testapp/lib/views/pages/course_page.dart
+++ testapp/lib/views/pages/course_page.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:testapp/views/pages/onboarding_page.dart';
+import 'package:testapp/data/classes/activity_class.dart';
 import 'package:testapp/widgets/hero_widgets.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  Future getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return Activity.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [HeroWidget(title: 'Course', nextPage: OnboardingPage())],
-          ),
-        ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          Widget widget;
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            widget = Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasData) {
+            Activity activity = snapshot.data;
+            widget = Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    HeroWidget(title: activity.activity),
+                    SizedBox(height: 20),
+                    Text(activity.activity),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            widget = Center(child: Text('Error'));
+          }
+          return widget;
+        },
       ),
     );
   }
